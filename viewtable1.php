ec7fd4db6dcc6c565be58f9f7d7d47c9dd6ae66b
chinkean023 <74999570+chinkean023@users.noreply.github.com>
2020-12-02 11:45:45 +0800

    Update viewtable1.php

--- viewtable1.php
+++ viewtable1.php
@@ -19,6 +19,23 @@
             <td>Menara Standard Chartered, TPM</td>
             <td>03-22441234</td>
             </tr>
+                
+            <?php
+                $serverName = "https://simplewebtp046245.azurewebsites.net";
+                $connectionOptions = array(
+                                     "Database" => "simpleweb",
+                                     "Uid" => "chinkean023",
+                                     "PWD" => "War.a.lew@023");
+                $conn = sqlsrv_connect($serverName, $connectionOptions);
+                if (!$conn)
+                {
+                        die("Error connection: ".sqlsrv_errors());
+                 } 
+                else
+                {
+                    echo "<script>alert('Success)';</script>";
+            ?>
+                
             </table>
         </center>
     </body>
